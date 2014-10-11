d1dafff53ec0889cd5bc2691acd86f30e78f7164
Seon-Wook Park <seon.wook@swook.net>
2014-10-11 17:55:00 +0200

    Search for best lambda in reducing range of values

--- project1/trainer.m
+++ project1/trainer.m
@@ -6,19 +6,34 @@ T = importData('training.csv');
 Nrows = size(T, 1);
 %T = (T - repmat(mean(T), Nrows, 1)) ./ repmat(var(T), Nrows, 1);
 
-lambdas = 0.002264:.00000001:.002265;
-errs = [];
+% Minimum step in searching for min-err-lambda
+lambda0 = 0;
+lambdaN = 100;
+dlambda = lambdaN;
+min_dlambda = 1e-7;
 
-for lambda = lambdas
-	lambda
-	errs = [errs crossValidation(T, lambda)];
+% Reduce range in which to search for value of lambda at minimum
+% estimated prediction error
+while dlambda > min_dlambda
+	dlambda = (lambdaN - lambda0) / 20;
+	lambdas = lambda0:dlambda:lambdaN;
+
+	errs = [];
+
+	for lambda = lambdas
+		lambda
+		errs = [errs crossValidation(T, lambda)];
+	end
+
+	% Select lambda with smallest estimated prediction errors
+	[min_value, min_index] = min(errs);
+	selected_lambda = lambdas(min_index);
+
+	lambda0 = max(selected_lambda - dlambda, 0);
+	lambdaN = selected_lambda + dlambda;
 end
 
 plot(lambdas, errs);
-
-% Select lambda with smallest estimated prediction errors
-[min_value, min_index] = min(errs);
-selected_lambda = lambdas(min_index);
 
 % Final predictors
 predictors = ridgeRegression(T, selected_lambda)
