1a00a2c99b221af99981988b6e55cbb3c5474040
Seon-Wook Park <seon.wook@swook.net>
2014-10-13 14:29:35 +0200

    Plot features against result

---
+++ project1/plotFeatures.m
@@ -0,0 +1,27 @@
+clear all;
+
+T = importData('training.csv');
+
+M = 3;
+N = 5;
+
+X = T(:, 1:end-1);
+Y = T(:, end);
+[Nrows, Ncols] = size(X);
+
+colTitles = {'\beta_0', 'Width', 'ROB size', 'IQ size', 'LSQ size',...
+	'RF sizes', 'RF read ports', 'RF write ports', 'Gshare size',...
+       	'BTB size', 'Branches allowed', 'L1 lcache size', 'L1 Dcache size',...
+	'L2 Ucache size', 'Depth'};
+
+for i = 1:Ncols
+	subplot(M, N, i);
+	plot(X(:, i), Y, '*');
+	title(colTitles(i));
+end
+
+fname = 'figures/ResultVsFeatures';
+saveas(gcf, fname, 'fig');
+saveas(gcf, fname, 'eps');
+saveas(gcf, fname, 'png');
+
