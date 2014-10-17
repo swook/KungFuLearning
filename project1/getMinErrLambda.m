499f18e10e1c819b39fa68886cd53345a0a53107
Seon-Wook Park <seon.wook@swook.net>
2014-10-17 10:57:45 +0200

    Make getMinErrLambda less spammy

--- project1/getMinErrLambda.m
+++ project1/getMinErrLambda.m
@@ -8,21 +8,22 @@ function selected_lambda = getMinErrLambda(T)
 	% Reduce range in which to search for value of lambda at minimum
 	% estimated prediction error
 	while dlambda > min_dlambda
-		dlambda = (lambdaN - lambda0) / 20;
+		dlambda = (lambdaN - lambda0) / 10;
 		lambdas = lambda0:dlambda:lambdaN;
 
 		errs = [];
 
+		disp(['Checking for lambda in ', num2str(lambda0), ' ~ ', num2str(lambdaN)]);
 		for lambda = lambdas
-			disp(lambda);
+			%disp(lambda);
 			errs = [errs crossValidation(T, lambda)];
-            
-%             figure(3)
-%             hold on
-%             predictors = ridgeRegression(T, lambda)
-%             plot(1:length(predictors),log10(predictors),'b')
-%             grid on
-%             hold on
+
+			%figure(3)
+			%hold on
+			%predictors = ridgeRegression(T, lambda)
+			%plot(1:length(predictors),log10(predictors),'b')
+			%grid on
+			%hold on
 		end
 
 		% Select lambda with smallest estimated prediction errors
@@ -31,19 +32,20 @@ function selected_lambda = getMinErrLambda(T)
 
 		lambda0 = max(selected_lambda - dlambda, 0);
 		lambdaN = selected_lambda + dlambda;
-%         figure(1)
-%         plot(lambdas, errs)
-        
-        
-    end
-%     figure(3)
-%     predictors = ridgeRegression(T,selected_lambda);
-%     h=plot(1:length(predictors),log10(predictors),'+-r')
-%     legend(h,'optimal \lambda');
-%     title('Variation of the predictors as a function of \lambda')
-%     ylabel('predictor value (logarithmic)')
-%     xlabel('predictor index')
-%     grid on
-%     hold off
-	
+		%figure(1)
+		plot(lambdas, errs)
+
+
+	end
+
+	%figure(3)
+	%predictors = ridgeRegression(T,selected_lambda);
+	%h=plot(1:length(predictors),log10(predictors),'+-r')
+	%legend(h,'optimal \lambda');
+	%title('Variation of the predictors as a function of \lambda')
+	%ylabel('predictor value (logarithmic)')
+	%xlabel('predictor index')
+	%grid on
+	%hold off
+
 end
