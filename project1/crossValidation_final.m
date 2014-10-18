3229d0631cc13c654228a5b3bbda9b18a026ce1f
Felix Renaut <frenaut@student.ethz.ch>
2014-10-18 13:12:20 +0200

    Above the hard baseline (8th)!!
    - tried adding an estimator of what our result would be when submitting
    + 1 submission

---
+++ project1/crossValidation_final.m
@@ -0,0 +1,92 @@
+function [ est_perr, var_perr ] = crossValidation_final(dataset, lambda)
+	% Inputs:
+	% - dataset: Training dataset
+	% - lambda:  Tuning parameter of Ridge Regression
+	%
+	% Output:
+	% - est_perr: Estimated prediction error
+
+	% No. of rows in datasets
+	N = size(dataset, 1);
+
+	% No. of training subsets
+	K = calculateNSubsets(dataset);
+
+	% No. of rows per training/validation subset
+	N_K = floor(N / (K + 1));
+
+	% No. of rows in last subset
+	N_L = N - N_K*K;
+
+	% List of prediction errors
+	perr_list = [];
+
+	% Pre-allocate T and V subsets
+	%T = zeros(N - N_K*K, size(dataset, 1));
+	%V = zeros(N_L, size(dataset, 1));
+
+	% For each validation subset
+	for i = 1:K % Position of validation subset
+
+		% Calculate range of rows of validation subset
+		i_Vstart = (i-1) * N_K + 1;
+		if i < K
+			i_Vend = i_Vstart + N_K - 1;
+		else
+			i_Vend = N;
+		end
+
+		% Training data subset
+		T = dataset([1:i_Vend-1 i_Vend+1:N], :);
+
+		% Validation data subset
+		V = dataset(i_Vstart:i_Vend, :);
+
+		% Run regression
+		predictors = ridgeRegression(T, lambda);
+
+		% Calculate prediction error
+		perr = predictionE_PS(predictors, V, lambda);
+		perr_list = [perr_list perr];
+	end
+
+	% Calculate estimate of prediction error
+	est_perr = mean(perr_list);
+    var_perr = var(perr_list);
+end
+
+function [K] = calculateNSubsets(dataset)
+	% Engineers' solution to finding no. of training subsets
+	%K = min(sqrt(size(dataset, 1)), 10);
+	K = floor(sqrt(size(dataset, 1)));
+end
+
+function err = predictionE_PS(predictors, V, lambda)
+	% Inputs:
+	% - predictors: Response of regression
+	% - V:          Validation dataset
+	% - lambda:     Tuning parameter of Ridge Regression
+	%
+	% Output:
+	% - err: Prediction error
+
+	[Nrows, Ncols] = size(V);
+
+	% If no feature present in dataset, error is infinite
+	if size(V, 2) < 2
+		err = inf;
+		return;
+	end
+
+	% Result from validation subset
+	realR = V(:, end);
+
+	% Calculate results from estimated predictors
+	estR = V(:, 1:end-1) * predictors;
+
+	% Calculate prediction error
+	dR = abs(realR - estR);
+    % Prediction error
+	err = sqrt(dot(dR, dR)/Nrows)/mean(realR);
+    
+end
--- project1/trainer_nonlinear_factors.m
+++ project1/trainer_nonlinear_factors.m
@@ -11,9 +11,9 @@ lambda = getMinErrLambda(T); % first calculation of lambda for comparison
 err_org = crossValidation(T,lambda); % original error
 display(['A first error estimation is ',num2str(err_org)])
 
-
-[T,paramCombi]=paramCombination(T,lambda);% T is extended with combinations of parameters
 [T,selected_factors] = getNonlinFactors(T,lambda); 
+[T,paramCombi]=paramCombination(T,lambda);% T is extended with combinations of parameters
+
 
 lambda_new = getMinErrLambda(T); % new specific calculation of optimal lambda
 
@@ -22,18 +22,23 @@ lambda_new = getMinErrLambda(T); % new specific calculation of optimal lambda
 predictors = ridgeRegression(T, lambda_new);
 prediction_error = crossValidation(T,lambda_new);
 display(['The prediction error is ',num2str(prediction_error)])
+[perr_PS,var_PS] = crossValidation_final(T,lambda_new);
+display(['The prediction error as calculated for the submission is ',num2str(perr_PS)])
+display(['The variance of this error is ',num2str(var_PS)])
+
 
 %V = csvread('validation.csv');
 V=importData('validation.csv');
 
 new_params=zeros(size(V,1),size(paramCombi,1));
+for i=1:size(V,2)
+    V(:,i)=V(:,i).^selected_factors(i); % add factors
+end
 for i=1:size(paramCombi,1)
     new_params(:,i)=V(:,paramCombi(i,1)).*V(:,paramCombi(i,2)); % combine parameters
 end
 V=[V,new_params]; %add new rows with combination of parameters at the end
-for i=1:size(V,2)
-    V(:,i)=V(:,i).^selected_factors(i); % add factors
-end
+
 
 estR = V*predictors;% estimation of time
 
