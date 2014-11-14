ddef13bbdc6dbdab390bd22450eca0db8f3bf914
Seon-Wook Park <seon.wook@swook.net>
2014-11-14 15:30:50 +0100

    Mess about with Felix

--- project2/src/gradientDescent.m
+++ project2/src/gradientDescent.m
@@ -1,15 +1,21 @@
-function [w] = gradientDescent(T, C)
+function [w] = gradientDescent(T, C, varargin)
     % Inputs:
     % - T: Testing set
     % - C: Hyperparameter
+    % - w0: Starting w (Optional)
     %
     % Output:
     % - w: Parameters
+
 
     [Nrows, Ncols] = size(T);
 
     % Initial w is zero-vector
     w = zeros(Ncols - 1, 1);
+
+    if length(varargin) > 2
+        w = varargin{3};
+    end
 
     % Classifications
     y = T(:, end);
@@ -19,17 +25,14 @@ function [w] = gradientDescent(T, C)
 
     i = 0;
     beta = 1;
-    err_limit = 1e-5;
+    err_limit = 1e-7;
     err = ones(size(w));
 
     grad = zeros(size(w));
     chk  = zeros(Nrows, 1); % [y_i * w' * x_i]
 
     % Until error sufficiently low
-    while norm(err, 2) > err_limit
-        %if mod(i, 1e3) == 0
-        %    disp(norm(err, 2));
-        %end
+    while true
 
         % Increment counter
         i = i + 1;
@@ -39,17 +42,21 @@ function [w] = gradientDescent(T, C)
         if C>=1
             beta=1/(i*C);
         end
+
         % Stochastic
-        idx = randi(Nrows);
         grad = 2 * w / Nrows;
 
-        if y(idx) * dot(w, x(idx, :)') < 1
-            if y(idx) > 0
-                grad = grad - C * y(idx) * x(idx, :)';
-            else
-                grad = grad - 5 * C * y(idx) * x(idx, :)';
+        %for i = 1:2
+            idx = randi(Nrows);
+
+            if y(idx) * dot(w, x(idx, :)') < 1
+                if y(idx) > 0
+                    grad = grad - 1 * C * y(idx) * x(idx, :)';
+                else
+                    grad = grad - 5 * C * y(idx) * x(idx, :)';
+                end
             end
-        end
+        %end
 
         %% Check per row
         %chk = y .* (x * w);
@@ -63,6 +70,14 @@ function [w] = gradientDescent(T, C)
         % Apply gradient descent
         err = beta * grad;
         w = w - err;
+
+        if norm(err, 2) < err_limit
+            break;
+        end
+
+        if mod(i, 1e1) == 0
+            disp(predictionE(w, T));
+        end
 
     end
 end
