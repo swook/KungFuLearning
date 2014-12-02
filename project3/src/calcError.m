6af610557d4f3db4a7dc038c94e68bb9d34d9e35
phineasng <anguyen@student.ethz.ch>
2014-12-02 20:44:42 +0100

    updated error function

--- project3/src/calcError.m
+++ project3/src/calcError.m
@@ -1,5 +1,11 @@
-function perr = calcError(Yhat_city,Yhat_country, Y_city, Y_country)
+function perr = calcError(Yhat, Y)
     % Yhat_* : predicted values
 	 % Y_* : true values
-    perr = sum(Yhat_city~=Y_city) + 0.25*sum(Yhat_country~=Y_country) 
+    Yhat_country = Yhat / 1000;
+    Yhat_city = mod(Yhat, 1000);
+    
+    Y_country = Y / 1000;
+    Y_city = mod(Y, 1000);
+    
+    perr = sum(Yhat_city~=Y_city) + 0.25*sum(Yhat_country~=Y_country);
 end
