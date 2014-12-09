c4d97f103cc05c31c8ae30e6ee98bcc0f86b5daa
Seon-Wook Park <seon.wook@swook.net>
2014-12-09 19:14:12 +0100

    Fix? adaBoost2

--- project3/src/adaBoost2.m
+++ project3/src/adaBoost2.m
@@ -3,19 +3,21 @@ function [models,alpha] = adaBoost2 (X,Y,Nclasses)
 [Nrows,Nfeats]=size(X);
 
 w=1/Nrows*ones(Nrows,1);
-c=zeros(Nclassifiers,Nclasses-1,Nfeats);
+%c=zeros(Nclasses-1,Nfeats);
 classifier=zeros(Nclasses-1,Nfeats);
-models=zeros(Nclassifiers,1);
+models=cell(Nfeats,1);
 Xweighted=X;
 for b=1:Nfeats
-	models(b) = svmtrain(Y, Xweighted(:,b),'-q'); % classify using one feature
-	outputs = svmpredict(ones(size(X, 1), 1), X(:,b), models(b));
-	wrongPredIdx=(X(:,b)==1 && Y~=outputs);
+        fprintf('%d\n', b);
+	models{b} = svmtrain(Y, Xweighted(:,b),'-q'); % classify using one feature
+        idx=find(X(:,b)>0);
+	outputs = svmpredict(ones(length(idx), 1), X(idx, b), models{b});
+	wrongPredIdx=(Y(idx)~=outputs);
 	%c(b,:)=models(b).sv_coef'*full(mode.SVs);
 	norm=sum(w);
 	epsilon=sum(w(wrongPredIdx));
 	alpha(b)=log((1-epsilon)/epsilon);
-	w(WrongPredIdx)=w(wrongPredIdx).*exp(alpha(b));
+	w(wrongPredIdx)=w(wrongPredIdx).*exp(alpha(b));
 	for i=1:Nrows
 		Xweighted(i,b)=Xweighted(i,b)*w(i);
 	end
